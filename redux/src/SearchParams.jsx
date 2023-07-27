9fb6f6cbaaec85a9e5875e863d864bd4dc946295
Quinth Anthony A. Razuman <chuckierazuman@gmail.com>
2023-07-27 22:54:01 +0800

    redux finished

--- redux/src/SearchParams.jsx
+++ redux/src/SearchParams.jsx
@@ -1,23 +1,20 @@
-import { useContext, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchQuery } from "./petApiService";
 import Results from "./Results";
-import AdoptedPetContext from "./AdoptedPetContext";
 import useBreedList from "./useBreedList";
-import fetchSearch from "./fetchSearch";
+import { all } from "./searchParamsSlice";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [requestParams, setRequestParams] = useState({
-    location: "",
-    animal: "",
-    breed: "",
-  });
-  const [adoptedPet] = useContext(AdoptedPetContext);
+  const adoptedPet = useSelector((state) => state.adoptedPet.value);
+  const searchParams = useSelector((state) => state.searchParams.value);
   const [animal, setAnimal] = useState("");
   const [breeds] = useBreedList(animal);
+  const dispatch = useDispatch();
 
-  const results = useQuery(["search", requestParams], fetchSearch);
-  const pets = results?.data?.pets ?? [];
+  let { data: pets } = useSearchQuery(searchParams);
+  pets = pets ?? [];
 
   return (
     <div className="search-params">
@@ -30,7 +27,7 @@ const SearchParams = () => {
             breed: formData.get("breed") ?? "",
             location: formData.get("location") ?? "",
           };
-          setRequestParams(obj);
+          dispatch(all(obj));
         }}
       >
         {adoptedPet ? (
--- redux/src/store.js
+++ redux/src/store.js
@@ -1,7 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import adoptedPet from "./adoptedPetSlice";
+import searchParams from "./searchParamsSlice";
+import { petApi } from "./petApiService";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    adoptedPet,
+    searchParams,
+    [petApi.reducerPath]: petApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(petApi.middleware),
 });
 
 export default store;
